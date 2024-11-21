69cfde548318524fd896d878f18d8e55b5da2594
affan50pro <sweet.baby12325@gmail.com>
2024-11-21 10:08:24 +0400

    first commit

---
+++ hard.py
@@ -0,0 +1,8 @@
+num=int(input("enter your number : "))
+sum=1
+i=1
+while i<=num:
+    sum=sum*i
+    i=i+1
+print("Sum is ",sum)
+#sir i dont know why the output of the code is like this but other than that it is working
